43a55bdb8e26e7b561fbd08c40cd7ecbdaf0517b
seonglae <sungle3737@gmail.com>
2023-10-13 04:08:57 +0900

    feat: re-indexing dps faiss to chromadb

--- index_ctx.py
+++ index_ctx.py
@@ -1,3 +1,5 @@
+import time
+
 import fire
 import chromadb
 
@@ -7,6 +9,16 @@ from resrer.retriever import DenseHNSWFlatIndexer
 def faiss_to_db(target="chroma-local", dpr_ctx="psgs_w100",
                 index_path="data/dpr/index", ctx_path="data/dpr/ctx",
                 save_steps=5000, chroma_path="data/chroma") -> str:
+  """Facebook DRP faiss index file to ChromaDB or other Vector DB 
+
+  Args:
+      target (str, optional): _description_. Defaults to "chroma-local".
+      dpr_ctx (str, optional): _description_. Defaults to "psgs_w100".
+      index_path (str, optional): _description_. Defaults to "data/dpr/index".
+      ctx_path (str, optional): _description_. Defaults to "data/dpr/ctx".
+      save_steps (int, optional): _description_. Defaults to 5000.
+      chroma_path (str, optional): _description_. Defaults to "data/chroma".
+  """
   indexer = DenseHNSWFlatIndexer()
   indexer.deserialize(index_path)
   print(
@@ -22,23 +34,32 @@ def faiss_to_db(target="chroma-local", dpr_ctx="psgs_w100",
   index_end = int(indexer.index_id_to_db_id[-1])
 
   with open(f"{ctx_path}/{dpr_ctx}.tsv", encoding='utf-8') as ctx_file:
+    faiss_id = 0
+    start = time.time()
     for i, line in enumerate(ctx_file):
+      if faiss_id == indexer.index.ntotal:
+        break
       if i < index_start:
         continue
       if i > index_end:
         continue
-      # Target passages in the index file
-      row = line.strip().split("\t")
-      id = row[0]
-      passage = row[1].strip('"')
-      if target == "chroma-local":
-        collection.upsert(ids=[id], embeddings=[
-                          indexer.index.reconstruct(i)], documents=[passage])
 
-      # Save db step
-      if i % save_steps == 0:
+      # Target passages in the index file
+      if int(indexer.index_id_to_db_id[faiss_id]) == i:
+        id = indexer.index_id_to_db_id[faiss_id]
+        row = line.strip().split("\t")
+        passage = row[1].strip('"')
         if target == "chroma-local":
-          print(f"Saving {i}th passage to {chroma_path}")
+          embedding = [float(item)
+                       for item in indexer.index.reconstruct(faiss_id)]
+          collection.upsert(ids=[id], embeddings=[
+                            embedding], documents=[passage])
+        # Save db step
+        if i % save_steps == 0:
+          if target == "chroma-local":
+            print(
+                f"Saving {faiss_id}th passage to {chroma_path} ({time.time() - start:.2f}s)")
+        faiss_id += 1
 
 
 if __name__ == '__main__':
