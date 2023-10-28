e58881be0cad9c8a2ee0342fcc8dfdc071016196
seonglaee <sungle3737@gmail.com>
2023-10-28 14:08:58 +0000

    feat: tei server indexing option with chroma-remote

--- index_ctx.py
+++ index_ctx.py
@@ -1,38 +1,58 @@
 import time
 from typing import Dict
+import json
+import asyncio, aiohttp
 
 import fire
 import chromadb
 from datasets import load_dataset, Dataset
 from huggingface_hub import HfApi
+import nest_asyncio
 
 from resrer.embedding import encode_hf
 from dpr.retriever import DenseHNSWFlatIndexer
 
+nest_asyncio.apply()
 
-def dataset(target="chroma-local", dataset_id="wikipedia",
+async def request(session: aiohttp.ClientSession, slug, data):
+  async with session.post(slug, data=json.dumps(data), headers={'Content-Type': 'application/json'}) as response:
+    res = await response.json()
+    if type(res) is not list: print(res)
+    return res[0]
+
+async def dataset(target="chroma-remote", dataset_id="wikipedia",
             model_id="thenlper/gte-small", user="seonglae",
             prefix="", subset='20220301.en', token=None, stream=False,
-            chroma_path="data/chroma", batch_size=10, start_index=None):
+            chroma_host="localhost", chroma_port='8000', tei=True,
+            chroma_path="data/chroma", batch_size=10, start_index=None, end_index=None):
   # Load DB and dataset
   if target == "chroma-local":
     db = chromadb.PersistentClient(f'{chroma_path}/{dataset_id}')
     collection = db.get_or_create_collection(dataset_id)
+  if target == "chroma-remote":
+    db = chromadb.HttpClient(chroma_host,chroma_port)
+    collection = db.get_or_create_collection(dataset_id)
   dataset = load_dataset(dataset_id, subset, streaming=stream)['train']
-  if not stream and start_index is not None:
-    dataset = dataset[start_index:]
+  if not stream and end_index is not None:
+    dataset = dataset[:int(end_index)]
     dataset = Dataset.from_dict(dataset)
-
+  if not stream and start_index is not None:
+    dataset = dataset[int(start_index):]
+    dataset = Dataset.from_dict(dataset)
   # Batch processing function
-  def batch_encode_hf(batch_data: Dict):
+  async def batch_encode(batch_data: Dict) -> Dict:
     start = time.time()
     batch_zip = zip(batch_data['id'], batch_data['title'],
                     batch_data['url'], batch_data['text'])
     rows = [{'id': row[0], 'title': row[1], 'url': row[2], 'text': row[3]}
             for row in batch_zip]
     input_texts = [f"{row['title']}\n{row['text']}" for row in rows]
-    embeddings = encode_hf(input_texts, model_id, prefix)
-    embeddings = [embedding.cpu().detach().numpy().tolist()
+    if tei:
+      async with aiohttp.ClientSession('http://localhost:8080') as session:
+        embeddings = await asyncio.gather(*[request(session, '/embed', {'inputs': text}) for text in input_texts])
+    else:
+      embeddings = encode_hf(input_texts, model_id, prefix)
+      embeddings = [embedding.cpu().detach().numpy().tolist()
                   for embedding in embeddings]
     metadatas = [{'title': row['title'], 'url': row['url']} for row in rows]
     collection.upsert(ids=batch_data['id'], embeddings=embeddings,
@@ -40,10 +60,10 @@ def dataset(target="chroma-local", dataset_id="wikipedia",
     print(
         f"Batched {len(batch_data['id'])}rows takes ({time.time() - start:.2f}s)")
     return {'embeddings': embeddings, 'query': input_texts}
-
+  def batcher(x):
+    asyncio.run(batch_encode(x))
   # Batch processing
-  batched = dataset.map(batch_encode_hf, batched=True, batch_size=batch_size)
-  list(batched)
+  dataset.map(batcher, batched=True, batch_size=batch_size)
 
   # Upload to Huggingface Hub
   if token is not None:
