a57fdd8fbd5d70228cc9a3ad5cd387a168e92f95
seonglae <sungle3737@gmail.com>
2023-11-07 12:48:38 +0900

    refactor: from chromadb to milvus because of scalability

--- index_ctx.py
+++ index_ctx.py
@@ -1,37 +1,45 @@
 import time
 from typing import Dict
 import json
-import asyncio, aiohttp
+import asyncio
+import aiohttp
 
 import fire
-import chromadb
+from pymilvus import MilvusClient, connections, db, Index, CollectionSchema, FieldSchema, DataType, MilvusException
 from datasets import load_dataset, Dataset
+from tei import TEIClient
 from huggingface_hub import HfApi
-import nest_asyncio
 
-from resrer.embedding import encode_hf
-from dpr.retriever import DenseHNSWFlatIndexer
 
-nest_asyncio.apply()
+def dataset(dataset_id="wiki_dpr", milvus_user='root', milvus_pw=None,
+            prefix="", subset='psgs_w100.nq.exact', stream=False,
+            milvus_host="localhost", milvus_port='19530', dim=768,
+            db_name="psgs_w100", collection_name='dpr_nq', tei=False,
+            tei_host="localhost", tei_port='8080', tei_protocol="http",
+            batch_size=100, start_index=None, end_index=None):
 
-async def request(session: aiohttp.ClientSession, slug, data):
-  async with session.post(slug, data=json.dumps(data), headers={'Content-Type': 'application/json'}) as response:
-    res = await response.json()
-    if type(res) is not list: print(res)
-    return res[0]
+  # Load DB
+  connections.connect(
+      host=milvus_host, port=milvus_port, user=milvus_user, password=milvus_pw)
 
-async def dataset(target="chroma-remote", dataset_id="wikipedia",
-            model_id="thenlper/gte-small", user="seonglae",
-            prefix="", subset='20220301.en', token=None, stream=False,
-            chroma_host="localhost", chroma_port='8000', tei=True,
-            chroma_path="data/chroma", batch_size=10, start_index=None, end_index=None):
-  # Load DB and dataset
-  if target == "chroma-local":
-    db = chromadb.PersistentClient(f'{chroma_path}/{dataset_id}')
-    collection = db.get_or_create_collection(dataset_id)
-  if target == "chroma-remote":
-    db = chromadb.HttpClient(chroma_host,chroma_port)
-    collection = db.get_or_create_collection(dataset_id)
+  if db_name not in db.list_database():
+    db.create_database(db_name)
+  client = MilvusClient(user=milvus_user, password=milvus_pw,
+                        uri=f"http://{milvus_host}:{milvus_port}", db_name=db_name)
+  if collection_name not in client.list_collections():
+    title = FieldSchema(name="title", dtype=DataType.STRING)
+    text = FieldSchema(name="text", dtype=DataType.STRING)
+    vec = FieldSchema(name="vec", dtype=DataType.FLOAT_VECTOR, dim=dim)
+    id_field = FieldSchema(name="id", dtype=DataType.VARCHAR,
+                           is_primary=True, max_length=8)
+    schema = CollectionSchema(
+        fields=[id_field, vec, title, text], enable_dynamic_field=True)
+    client.create_collection(
+        schema=schema, collection_name=collection_name, id_type='string', dimension=dim, max_length=8)
+    Index(collection_name, field_name='vec', index_params={
+        'index_type': 'HNSW', 'index_param': {'M': 32, 'efConstruction': 1024}, 'ef': 8192})
+
+  # Load dataset
   dataset = load_dataset(dataset_id, subset, streaming=stream)['train']
   if not stream and end_index is not None:
     dataset = dataset[:int(end_index)]
@@ -39,113 +47,32 @@ async def dataset(target="chroma-remote", dataset_id="wikipedia",
   if not stream and start_index is not None:
     dataset = dataset[int(start_index):]
     dataset = Dataset.from_dict(dataset)
+
   # Batch processing function
-  async def batch_encode(batch_data: Dict) -> Dict:
+  if tei:
+    teiclient = TEIClient(host=tei_host, port=tei_port, protocol=tei_protocol)
+
+  def batch_encode(batch_data: Dict) -> Dict:
     start = time.time()
-    batch_zip = zip(batch_data['id'], batch_data['title'],
-                    batch_data['url'], batch_data['text'])
-    rows = [{'id': row[0], 'title': row[1], 'url': row[2], 'text': row[3]}
+    batch_zip = zip(batch_data['id'], batch_data['title'], batch_data['text'])
+    print(batch_data.keys())
+    rows = [{'id': row[0], 'title': row[1], 'text': row[2]}
             for row in batch_zip]
-    input_texts = [f"{row['title']}\n{row['text']}" for row in rows]
-    if tei:
-      async with aiohttp.ClientSession('http://localhost:8080') as session:
-        embeddings = await asyncio.gather(*[request(session, '/embed', {'inputs': text}) for text in input_texts])
+    input_texts = [f"{prefix}{row['title']}\n{row['text']}" for row in rows]
+    if batch_data['embeddings'] is not None:
+      embeddings = batch_data['embeddings']
     else:
-      embeddings = encode_hf(input_texts, model_id, prefix)
-      embeddings = [embedding.cpu().detach().numpy().tolist()
-                  for embedding in embeddings]
-    metadatas = [{'title': row['title'], 'url': row['url']} for row in rows]
-    collection.upsert(ids=batch_data['id'], embeddings=embeddings,
-                      documents=batch_data['text'], metadatas=metadatas)
+      embeddings = teiclient.embed_batch_sync(input_texts)
+    for i, row in enumerate(rows):
+      row['vector'] = embeddings[i]
+    client.insert(collection_name=collection_name, records=rows)
     print(
         f"Batched {len(batch_data['id'])}rows takes ({time.time() - start:.2f}s)")
     return {'embeddings': embeddings, 'query': input_texts}
-  def batcher(x):
-    asyncio.run(batch_encode(x))
+
   # Batch processing
-  dataset.map(batcher, batched=True, batch_size=batch_size)
-
-  # Upload to Huggingface Hub
-  if token is not None:
-    api = HfApi(token=token)
-    api.create_repo(f'{user}/chroma-{dataset_id}',
-                    repo_type="dataset", exist_ok=True)
-    api.upload_folder(
-        folder_path=f'{chroma_path}/{dataset_id}',
-        repo_id=f"{user}/chroma-{dataset_id}",
-        repo_type="dataset",
-    )
-
-
-def faiss(target="chroma-remote", ctx_name="psgs_w100", ctx_ext="tsv",
-          chroma_host="localhost", chroma_port='1234',
-          index_path="data/dpr/index", ctx_path="data/dpr/ctx", token=None, user="seonglae",
-          save_steps=5000, chroma_path="data/chroma", start_index: int = None, end_index: int = None) -> str:
-  """Facebook DRP faiss index file to ChromaDB or other Vector DB 
-  """
-  # Load faiss index
-  start = time.time()
-  indexer = DenseHNSWFlatIndexer()
-  indexer.deserialize(index_path)
-  print(
-      f"Index documents: {indexer.index.ntotal}, Index dimension: {indexer.index.d}, Load time: {time.time() - start:.2f}s")
-  int_index_id_to_db_id = [int(db_id) for db_id in indexer.index_id_to_db_id]
-  index_map = {db_id: index_id for index_id,
-               db_id in enumerate(int_index_id_to_db_id)}
-  sorted_index = sorted(int_index_id_to_db_id)
-
-  # DB initialization
-  if target == "chroma-local":
-    db = chromadb.PersistentClient(f'{chroma_path}/{ctx_name}')
-    collection = db.get_or_create_collection(ctx_name)
-  if target == "chroma-remote":
-    db = chromadb.HttpClient(chroma_host,chroma_port)
-    collection = db.get_or_create_collection(ctx_name)
-
-  # Get the faiss index starting point
-  index_start, index_end = int(sorted_index[0]), int(sorted_index[-1])
-  if start_index is not None:
-    index_start = int(start_index)
-  if end_index is not None:
-    index_end = int(end_index)
-  print(f"Index start: {index_start}, Index end: {index_end}")
-
-  # Read the context file per line
-  with open(f"{ctx_path}/{ctx_name}.{ctx_ext}", 'r', encoding='utf-8') as ctx_file:
-    start = time.time()
-    for i, line in enumerate(ctx_file):
-      if i < index_start:
-        continue
-      if i > index_end:
-        print(
-            f"End: {i} ({time.time() - start:.2f}s)")
-        break
-
-      # Target passages in the index file
-      if i in index_map:
-        index_id = index_map[i]
-        row = line.strip().split("\t")
-        passage = row[1].strip('"')
-        title = row[2].strip('"')
-        embedding = [float(item)
-                     for item in indexer.index.reconstruct(index_id)]
-        collection.upsert(ids=[str(i)], embeddings=[
-                          embedding], documents=[passage], metadatas=[{'title': title}])
-        # Save db step
-        if i % save_steps == 0:
-          print(
-                f"Saving {i}th passage from db id {index_id} to {chroma_path} ({time.time() - start:.2f}s)")
-
-  # Upload to Huggingface Hub
-  if token is not None:
-    api = HfApi(token=token)
-    api.create_repo(f'{user}/chroma-{ctx_name}',
-                    repo_type="dataset", exist_ok=True)
-    api.upload_folder(
-        folder_path=f'{chroma_path}/{ctx_name}',
-        repo_id=f"{user}/chroma-{ctx_name}",
-        repo_type="dataset",
-    )
+  batched = dataset.map(batch_encode, batched=True, batch_size=batch_size)
+  return batched
 
 
 if __name__ == '__main__':
