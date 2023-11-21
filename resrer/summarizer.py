27778c870afdcb448b466964801bb996bfe86e0c
seonglae <sungle3737@gmail.com>
2023-11-21 20:02:33 +0900

    refactor: change from pegasus-x to bert for performance

--- resrer/summarizer.py
+++ resrer/summarizer.py
@@ -1,9 +1,9 @@
 from typing import List, Tuple
-from transformers import AutoTokenizer, PegasusXForConditionalGeneration, PegasusTokenizer
+from transformers import AutoTokenizer, BartForConditionalGeneration, BartTokenizerFast
 import torch
 
 
-def summarize_text(tokenizer: PegasusTokenizer, model: PegasusXForConditionalGeneration,
+def summarize_text(tokenizer: BartTokenizerFast, model: BartForConditionalGeneration,
                    input_texts: List[str]):
   inputs = tokenizer(input_texts, padding=True,
                      return_tensors='pt', truncation=True).to(1)
@@ -14,9 +14,9 @@ def summarize_text(tokenizer: PegasusTokenizer, model: PegasusXForConditionalGen
   return summaries
 
 
-def get_summarizer(model_id="pszemraj/pegasus-x-large-book-summary") -> Tuple[PegasusTokenizer, PegasusXForConditionalGeneration]:
-  tokenizer = AutoTokenizer.from_pretrained(model_id)
-  model = PegasusXForConditionalGeneration.from_pretrained(model_id).to(1)
+def get_summarizer(model_id="ccdv/lsg-bart-base-4096-multinews") -> Tuple[BartTokenizerFast, BartForConditionalGeneration]:
+  tokenizer = BartTokenizerFast.from_pretrained(model_id)
+  model = BartForConditionalGeneration.from_pretrained(model_id).to(1)
   model = torch.compile(model)
   return tokenizer, model
 
