73c36828f1f8992a7b391e4e026c88d48657dd36
seonglae <sungle3737@gmail.com>
2023-11-23 03:19:48 +0000

    fix: min max length for summarizer for bart

--- resrer/summarizer.py
+++ resrer/summarizer.py
@@ -8,7 +8,7 @@ def summarize_text(tokenizer: Union[PegasusTokenizerFast, BartTokenizerFast],
                    model: Union[PegasusXForConditionalGeneration, BartForConditionalGeneration],
                    input_texts: List[str]):
   inputs = tokenizer(input_texts, padding=True,
-                     return_tensors='pt', truncation=True).to(1)
+                     return_tensors='pt', truncation=True).to(0)
   with torch.backends.cuda.sdp_kernel(enable_flash=True, enable_math=False, enable_mem_efficient=False):
     summary_ids = model.generate(inputs["input_ids"])
   summaries = tokenizer.batch_decode(summary_ids, skip_special_tokens=True,
@@ -22,11 +22,13 @@ def get_summarizer(model_id="seonglae/resrer-bart-base") -> Tuple[Union[PegasusT
                                                                         BartForConditionalGeneration]]:
   if 'bart' in model_id:
     tokenizer = BartTokenizerFast.from_pretrained(model_id)
-    model = BartForConditionalGeneration.from_pretrained(model_id).to(1)
+    model = BartForConditionalGeneration.from_pretrained(
+        model_id, min_length=256, max_length=512).to(0)
   elif 'pegasus' in model_id:
     tokenizer = PegasusTokenizerFast.from_pretrained(model_id)
-    model = PegasusXForConditionalGeneration.from_pretrained(model_id).to(1)
+    model = PegasusXForConditionalGeneration.from_pretrained(
+        model_id, min_length=256, max_length=512).to(0)
   model = torch.compile(model)
   return tokenizer, model
 
-# OpenAI reader
+# OpenAI summarizer
