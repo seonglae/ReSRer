5bb525edff5299210827890aa24b418f7eec1e57
seonglae <sungle3737@gmail.com>
2023-11-30 14:30:31 +0900

    feat: train with special separator token

--- resrer/train.py
+++ resrer/train.py
@@ -1,20 +1,21 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer, DataCollatorForSeq2Seq
-import numpy as np
 import torch
 from huggingface_hub import login
-from dotenv import dotenv_values
 
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
 
-config = dotenv_values(".env")
 
-def preprocesser(tokenizer):
+def preprocesser(tokenizer, special_token=False):
   def preprocess_function(examples):
-    inputs = [f"{examples['question_text'][i]}\n{doc}" for i,
-              doc in enumerate(examples["document_text"])]
+    if special_token:
+      inputs = [f"{examples['question_text'][i]}<sep>{doc}" for i,
+                doc in enumerate(examples["document_text"])]
+    else:
+      inputs = [f"{examples['question_text'][i]}\n{doc}" for i,
+                doc in enumerate(examples["document_text"])]
     model_inputs = tokenizer(inputs, truncation=True)
     labels = tokenizer(
         text_target=examples["summarization_text"], truncation=True)
@@ -23,8 +24,8 @@ def preprocesser(tokenizer):
   return preprocess_function
 
 
-def training(output='seonglae/resrer-bart-base', dataset_id='seonglae/resrer-nq', checkpoint='facebook/bart-base',
-             token=config['HF_TOKEN']):
+def training(output, dataset_id, checkpoint, batch_size=4, special_token=False,
+             token=None, learning_rate=2e-5, weight_decay=0.01, epochs=4):
   if token is not None:
     login(token=token)
   # Load model
@@ -32,30 +33,31 @@ def training(output='seonglae/resrer-bart-base', dataset_id='seonglae/resrer-nq'
 
   # Load dataset
   dataset = load_dataset(dataset_id, split='train')
-  splited_dataset = dataset.train_test_split(test_size=0.2)
-  tokenized_dataset = splited_dataset.map(
-      preprocesser(tokenizer), batched=True)
-  print(tokenized_dataset["train"][0])
+  tokenized_dataset = dataset.map(
+      preprocesser(tokenizer, special_token), batched=True)
+  print(tokenized_dataset)
 
   # Train
   model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+  # Separator Token
+  if special_token:
+    tokenizer.add_tokens(['<sep>'])
+    model.resize_token_embeddings(len(tokenizer))
   data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=checkpoint)
   training_args = Seq2SeqTrainingArguments(
       output_dir=output,
-      evaluation_strategy="epoch",
-      learning_rate=2e-5,
-      per_device_train_batch_size=4,
+      learning_rate=learning_rate,
+      per_device_train_batch_size=batch_size,
       optim='adamw_hf',
-      weight_decay=0.01,
+      weight_decay=weight_decay,
       save_total_limit=3,
-      num_train_epochs=4,
+      num_train_epochs=epochs,
       push_to_hub=True,
   )
   trainer = Seq2SeqTrainer(
       model=model,
       args=training_args,
-      train_dataset=tokenized_dataset["train"],
-      eval_dataset=tokenized_dataset["test"],
+      train_dataset=tokenized_dataset,
       tokenizer=tokenizer,
       data_collator=data_collator,
   )
--- train.py
+++ train.py
@@ -1,10 +1,15 @@
 import fire
+from dotenv import dotenv_values
 
 from resrer.train import training
 
+config = dotenv_values(".env")
 
-def train(token):
-  training(token=token)
+
+def train(output='seonglae/resrer-bart', dataset_id='seonglae/resrer-nq', batch_size=8, epochs=4,
+          checkpoint='facebook/bart-base', token=config['HF_TOKEN'], special_token=False):
+  training(output=output, dataset_id=dataset_id, batch_size=batch_size, epochs=epochs,
+           checkpoint=checkpoint, token=token, special_token=special_token)
 
 
 if __name__ == '__main__':
